e1d11711db1f607de4ec1cc31d0f8cd3659d6966
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-12-03 19:11:40 +0300

    Add files via upload

---
+++ GP-1/Gorsel2-3.11/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gorsel2_3._11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rasgele = new Random();
+            int rnd = rasgele.Next(1,listBox1.Items.Count+1);
+            string kisi = listBox1.Items[rnd - 1].ToString();
+            MessageBox.Show(kisi);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+    }
+}
