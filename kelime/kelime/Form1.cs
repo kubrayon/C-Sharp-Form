51313e56eef9549da920c2625db08c1a7da20a7e
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-12-03 20:20:10 +0300

    Add files via upload

---
+++ kelime/kelime/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kelime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        kubra kbr = new kubra();
+        string kelime="";
+        int hak = 4;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked==true)
+            {
+                kelime = kbr.kelimegetir(true);
+                label1.Text = kbr.kelimegizle(kelime);
+            }
+            else
+            {
+                kelime = kbr.kelimegetir(false);
+                label1.Text = kbr.kelimegizle(kelime);
+            }   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (hak>0)
+            {
+                label1.Text = kbr.dene(textBox1.Text, kelime, label1.Text);
+                hak--;
+            }
+            else
+            {
+                MessageBox.Show("Hak bitti");
+                textBox1.Visible = false;
+                button2.Visible = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (kbr.tahmin(kelime,textBox2.Text))
+            {
+                MessageBox.Show("Tahmin yanlış!");
+            }
+            else
+            {
+                MessageBox.Show("Tahmin doğru!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = new DialogResult();
+            dialog = MessageBox.Show("Programdan çıkılsın mı?", "Programı Kapat", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Çıkış yapılmadı");
+            }
+        }
+    }
+}
---
+++ kelime/kelime/kubra.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace kelime
+{
+    class kubra
+    {
+        SqlConnection bag = new SqlConnection("server=PC-BILGISAYAR\\SQLEXPRESS;initial catalog = kelimetahmin; integrated security = true;");
+        public string kelimegetir(bool en)
+        {
+            string sql = "select tr, en from kelimeler";
+            int index;
+            int kelime_sayisi;
+            SqlDataAdapter da = new SqlDataAdapter(sql, bag);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            kelime_sayisi = tablo.Rows.Count;
+            Random rasgele = new Random();
+            index = rasgele.Next(0, kelime_sayisi);
+            if (en == true)
+                return tablo.Rows[index][1].ToString();
+            else
+                return tablo.Rows[index][0].ToString();
+        }
+        public string kelimegizle(string kelimem)
+        {
+            kelimem = kelimem.Trim();
+            string gizli = "";
+            for (int i = 0; i < kelimem.Length; i++)
+            {
+                gizli += "*";
+            }
+            return gizli;
+        }
+        public string dene(string harf,string kelime,string gizli)
+        {
+            string yeni="";
+            kelime = kelime.Trim();
+            for (int i = 0; i < kelime.Length; i++)
+            {
+                if (kelime[i].ToString() == harf.ToString())
+                {
+                    yeni += harf.ToString();
+                }
+                else
+                {
+                    yeni += gizli[i].ToString();
+                }   
+            }
+            return yeni;
+        }
+        public bool tahmin(string kelime,string tahmin)
+        {
+            if (kelime == tahmin)
+                return true;
+            else
+                return false;
+        }
+    }
+}
