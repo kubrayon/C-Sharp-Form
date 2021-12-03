34c8e997d1d72dd4faa3faa0646cb55f30b39844
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-12-03 19:35:57 +0300

    Add files via upload

---
+++ Uygulama-4/GP/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                comboBox1.Items.Add(i);
+                button1.Text = "Göster";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = " ";
+            int sayi =Convert.ToInt32( comboBox1.Text);
+            int cevap = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                cevap = i * sayi;
+                label1.Text += sayi.ToString() + "X" + i.ToString() + "=" + cevap.ToString() +"\n";
+
+            }
+
+        }
+    }
+}
