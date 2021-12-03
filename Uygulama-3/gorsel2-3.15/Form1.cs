ab96ccd728452a11138c937b8dd4c125ee802318
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-12-03 19:21:26 +0300

    Add files via upload

---
+++ Uygulama-3/gorsel2-3.15/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gorsel2_3._15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*UYgulama: Textboxa girilen sayının faktöriyelini çarpmaları göstererek labela yazdıran kodu yazınızint  */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(textBox1.Text);
+            int sonuc = 1;
+            for (int i = sayi; i >=1; i--)
+            {
+                sonuc = sonuc * i;
+                if (i==1)
+                {
+                    label1.Text += i.ToString() + " =  "+sonuc;
+
+                }
+                else
+                {
+                     label1.Text += i.ToString() +" X  ";
+                }
+               
+            }
+        }
+    }
+}
