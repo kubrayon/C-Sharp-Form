edbe1128280046c1456bd74e32525bf643d25fc6
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-12-03 20:16:27 +0300

    Add files via upload

---
+++ Yazı Tura/WindowsFormsApp26/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApp26
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yazı Tura At";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(75, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Yazı Tura/WindowsFormsApp26/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp26
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+/*Uygulama 6: Aşağıdaki form görüntüsünü oluşturunuz.
+* Butona basınca rasgele yazı tura atan kodu yapınız */
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Random rasgele = new Random();
+            int gelen = rasgele.Next(1,3);
+            if (gelen==1)
+            {
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpg");System.Threading.Thread.Sleep(200);
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg"); System.Threading.Thread.Sleep(200);
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpg"); System.Threading.Thread.Sleep(200);
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg"); System.Threading.Thread.Sleep(200);
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpg");
+
+            }
+            else
+            {
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg"); System.Threading.Thread.Sleep(200);
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpg"); System.Threading.Thread.Sleep(200);
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg"); System.Threading.Thread.Sleep(200);
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpg"); System.Threading.Thread.Sleep(200);
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg");
+
+            }
+        }
+    }
+}
